290f8ce98786bb4a725d37c413c38fd40f2e8d9a
Shivam kumar <shivamkumarrnc25@gmail.com>
2026-04-02 11:04:38 +0530

    Fix Cloudinary env variable and upload issue

--- backend/cloudConfig.js
+++ backend/cloudConfig.js
@@ -4,7 +4,7 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
